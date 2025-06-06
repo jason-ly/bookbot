70075b882b41976f5c389c8cc56fa62b0014e846
jason-ly <jas0nly@outlook.com>
2025-06-06 15:06:12 -0700

    Added sys.argv module to process command line arguments to allow bookbot to accept more books.

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from stats import word_count
+import sys
+from stats import (
+   word_count, 
+   character_count,
+   sorted_dict,
+)
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1) #Exit the program with a non-zero status to inidcate an error.
+    book_path = sys.argv[1] #Uses the second argument as the book path.
     text = get_book_text(book_path)
-    # print(text) - previous part
-    print(f"{word_count(text)} words found in the document")
+    num_words = word_count(text)
+    char_counts = character_count(text)
+    sorted_chars = sorted_dict(char_counts)
+    print_report(book_path, num_words, sorted_chars)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def print_report(book_path, num_words, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        if not item["char"].isalpha():
+                continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,32 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+def character_count(text):
+    total_character = text.lower()
+    char_count = {}
+
+    for c in total_character:
+        if c in char_count:
+            char_count[c] += 1
+        else: 
+            char_count[c] = 1
+
+    return char_count
+
+def sort_on(d):
+    return d["num"]
+
+def sorted_dict(char_count):
+    new_dict = []
+
+    for char in char_count:
+        new_dict.append({"char" : char, "num" : char_count[char]})
+    
+    new_dict.sort(reverse=True, key=sort_on)
+    
+    return new_dict
+
+
+
+
